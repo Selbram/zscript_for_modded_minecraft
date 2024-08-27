c2daba3c5297ee3275ea9aae5d90f8a6c69182b6
Selbramwolf <tclement@selbram.org>
2024-08-26 22:29:56 -0400

    Added dragon_breath.zs

--- peaceful_recipes_for_hostile_mob_drops/hostile_drops_recipies_for_peaceful.rs
+++ peaceful_recipes_for_hostile_mob_drops/hostile_drops_recipies_for_peaceful.rs
@@ -12,9 +12,9 @@ Nether stars (sea lantern middle, diamond block corners, echo shard gap)
 Bones (from bone meal obtained from a composter) (diagnol bone meal)
 Tridents (prismarine shaft, sea lantern forks)
 Totems of Undying (ghast tear center bottom, emerald block right and left, rest gold block)
-
 Sponges (water bucket center, fish corner, white wool bottom, green woold left and right, yellow wool up)
 Dragon breath (potion bottle, two popped chorus, gunpowder)
+
 Honeycomb (6 honey block, 3 bonemeal)
 Rabbit Hide (honeycomb center, paper corner, string gap)
 Honey Bottle (potion, two sugar, flower)
---
+++ peaceful_recipes_for_hostile_mob_drops/minecraft/dragon_breath.zs
@@ -0,0 +1,17 @@
+# Attempt at creating a zscript to make playing peaceful less
+# painful. 
+# - Selbramwolf
+# Inspired by Vegan Hostile Mob Drops Mod by notblue on modrinth.
+
+import crafttweaker.api.tag.MCTag;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.ingredient.IIngredient;
+import crafttweaker.api.item.ItemDefinition;
+import stdlib.List;
+
+craftingTable.addShapeless(
+    "dragon_breath", <item:minecraft:dragon_breath>, [
+        <tag:item:c:potions>, <item:minecraft:popped_chorus_fruit>, <item:minecraft:popped_chorus_fruit>, <item:minecraft:gunpowder>
+    ]
+);
+
